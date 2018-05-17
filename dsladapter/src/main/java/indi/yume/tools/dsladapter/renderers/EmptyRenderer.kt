6838b3082333779f2078ebf640737560b223d72a
xuemao.tang <xuemao.tang@fenrir-inc.com.cn>
2018-05-17 19:52:15 +0800

    Change EmptyRenderer to implements BaseRenderer

--- dsladapter/src/main/java/indi/yume/tools/dsladapter/renderers/EmptyRenderer.kt
+++ dsladapter/src/main/java/indi/yume/tools/dsladapter/renderers/EmptyRenderer.kt
@@ -3,6 +3,7 @@ package indi.yume.tools.dsladapter.renderers
 import android.support.v7.widget.RecyclerView
 import android.view.ViewGroup
 import indi.yume.tools.dsladapter.datatype.UpdateActions
+import indi.yume.tools.dsladapter.typeclass.BaseRenderer
 import indi.yume.tools.dsladapter.typeclass.Renderer
 import indi.yume.tools.dsladapter.typeclass.ViewData
 
@@ -10,14 +11,14 @@ import indi.yume.tools.dsladapter.typeclass.ViewData
  * Created by yume on 18-3-20.
  */
 
-class EmptyRenderer<T> : Renderer<T, EmptyViewData> {
+class EmptyRenderer<T> : BaseRenderer<T, EmptyViewData>() {
     override fun getData(content: T): EmptyViewData = EmptyViewData
 
     override fun getItemViewType(data: EmptyViewData, position: Int): Int =
             throw UnsupportedOperationException("EmptyRenderer do not support getItemViewType.")
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
-            throw UnsupportedOperationException("EmptyRenderer do not support onCreateViewHolder.")
+    override fun getLayoutResId(data: EmptyViewData, position: Int): Int =
+            throw UnsupportedOperationException("EmptyRenderer do not support getLayoutResId.")
 
     override fun bind(data: EmptyViewData, index: Int, holder: RecyclerView.ViewHolder) =
             throw UnsupportedOperationException("EmptyRenderer do not support bind.")
